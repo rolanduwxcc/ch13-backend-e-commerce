da6475e52fe2a6b0912bd3fc73a99a0f0e2101a0
rolanduwxcc <rolanduwxcc@gmail.com>
2021-03-13 01:15:02 -0600

    category routes working except for the POST/UPDATE one

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,72 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'product_name',
+          'price',
+          'stock'
+        ]
+      }
+    ]
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'product_name',
+          'price',
+          'stock'
+        ]
+      }
+    ]
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  console.log(req);
+  Category.update(req.body.category_name, {
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategory => {
+    res.json(dbCategory);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((dbCategory) => {
+    res.json(dbCategory);
+  });
 });
 
 module.exports = router;
